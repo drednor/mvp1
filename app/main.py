bd512a526aa0c9a80d84719c48546798091681c5
Abhishek Singh <singh.msoe@gmail.com>
2023-11-22 17:00:14 +0530

    added dockerfile and all files under app dir

--- app/main.py
+++ app/main.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+import os
 
 app = FastAPI()
-templates = Jinja2Templates(directory="templates")
+templates = Jinja2Templates(directory=os.path.join(
+    os.path.dirname(os.path.abspath(__file__)), "templates"))
 
 
 @app.get("/home")
